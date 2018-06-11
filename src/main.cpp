13b045d7642194ced3687263cf73827559d7bd82
Jacek Czastkiewicz <jacek.czastkiewicz@capgemini.com>
2018-06-11 20:02:52 +0200

    [master]

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,48 @@
 #include <Arduino.h>
+#include <DHT.h>
 
 const int LED_PIN = D1;
+
+const int DHT_PIN = D2;
+const int DHT_TYPE = DHT11;
+
+DHT dht(DHT_PIN, DHT_TYPE);
 
 void setup()
 {
   Serial.begin(9600);
 
   pinMode(LED_PIN, OUTPUT);
+
+  dht.begin();
+}
+
+void printAll(float h, float t, float f, float hic, float hif)
+{
+  char buffer[100];
+  sprintf(buffer, "Humidity: %.01f %%, Temperature: %.01f *C %.01f *F, Heat index: %.01f *C %.01f *F", h, t, f, hic, hif);
+  Serial.println(buffer);
 }
 
 void loop()
 {
-  Serial.println("LED on");
   digitalWrite(LED_PIN, HIGH);
   delay(1000);
-  Serial.println("LED off");
   digitalWrite(LED_PIN, LOW);
   delay(1000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+
+  if (isnan(h) || isnan(t) || isnan(f))
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  float hif = dht.computeHeatIndex(f, h);
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  printAll(h, t, f, hic, hif);
 }
