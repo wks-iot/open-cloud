173ff8d710263a8a9a822ec785035ed4b17ea6ac
Maciej Koscielski <maciej.koscielski@capgemini.com>
2018-07-12 17:20:28 +0200

    The first version of the exercise with MicroSwitch and LED

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,26 @@
 #include <Arduino.h>
 
-const int LED_PIN = D1;
-const int ANALOG_PIN = A0;
-const int LIMIT_VALUE = 750;
-int analogValue = 0;
+int PIN_BUTTON = D1;
+int PIN_LED = D2;
+
+boolean ledState = false;
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(ANALOG_PIN, INPUT);
+  pinMode(PIN_BUTTON, INPUT_PULLUP);
+  pinMode(PIN_LED, OUTPUT);
 }
 
 void loop()
 {
-  analogValue = analogRead(A0);
-  Serial.println(analogValue);
-  if(analogValue < LIMIT_VALUE){
-    digitalWrite(LED_PIN, 1);
-  } else {
-    digitalWrite(LED_PIN, 0);
+  if (!digitalRead(PIN_BUTTON))
+  {
+    Serial.println("CLICK!");
+    ledState = !ledState;
+    digitalWrite(PIN_LED, ledState);
+    Serial.println(ledState);
+    delay(1000);
   }
-  delay(200);
+  delay(10);
 }
