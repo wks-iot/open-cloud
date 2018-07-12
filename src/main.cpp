30ced5be4aa76cd5f244a9e6def6aad38af59c79
Maciej Koscielski <maciej.koscielski@capgemini.com>
2018-07-12 16:12:05 +0200

    The first version of the exercise with LED and photoresistor

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,33 @@
 #include <Arduino.h>
 
 const int LED_PIN = D1;
+const int PHOTORESISTOR_PIN = D2;
+
+int result;
+int oldresult;
 
 void setup()
 {
   Serial.begin(9600);
-
   pinMode(LED_PIN, OUTPUT);
+  pinMode(PHOTORESISTOR_PIN, INPUT);
 }
 
 void loop()
 {
-  Serial.println("LED on");
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  Serial.println("LED off");
-  digitalWrite(LED_PIN, LOW);
-  delay(1000);
+  oldresult = result;
+  result = digitalRead(D2);
+  if (result != oldresult)
+  {
+    digitalWrite(LED_PIN, result);
+    if (result == 0)
+    {
+      Serial.println("LED OFF");
+    }
+    else
+    {
+      Serial.println("LED ON");
+    }
+  }
+  delay(10);
 }
